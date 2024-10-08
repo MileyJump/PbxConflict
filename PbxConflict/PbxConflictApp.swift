b151323d576b63c23ea59efa0a54d6c01cd6f8c9
choiminkyung <choialsrud2002@gmail.com>
2024-10-08 11:14:17 +0900

    Initial Commit

---
+++ PbxConflict/PbxConflictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PbxConflictApp.swift
+//  PbxConflict
+//
+//  Created by 최민경 on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct PbxConflictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
